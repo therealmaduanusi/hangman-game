ba26caeeb0f5f2f682d6f0dac6a1f63b1a4c2482
Victor Maduanusi <maduanusivictor2020@gmail.com>
2025-05-09 15:17:36 +0100

    working on the utility style class

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,19 +7,19 @@ function Header({selectedCategory}) {
   console.log('header');
   
   return (
-    <div className={`flex justify-between`}>
-      <div className={`flex gap-x-[1rem] `}>
-        <button className={`self-center bg-gradient-to-r from-[#FE71FE] to-[#7199FF] shadow-pink-sh p-[1.5rem] max-md:p-[1rem] rounded-[50%]`}>
-          <img className='max-w-[100%]' src={menuImg} alt="menu or pause" />
+    <section className={`flex justify-between gap-x-[1rem]`}>
+      <div className={`flex gap-x-[1rem] w-[50%]`}>
+        <button className={`self-center flex justify-center bg-gradient-to-r from-[#FE71FE] to-[#7199FF] shadow-pink-sh rounded-[50%] p-[0.9rem]`}>
+          <img className='self-center' src={menuImg} alt="menu or pause" />
         </button>
-        <h1 className='text-[#fff] text-[4.5rem] max-md:text-[3rem] self-center'>{selectedCategory}</h1>
+        <h1 className='text-[#fff] text-[2.7rem] max-md:text-[1.5rem] self-center'>{selectedCategory}</h1>
       </div>
       {/* Healths */}
-      <div className={`flex`}>
+      <div className={`flex w-[50%] gap-x-[10px] self-center justify-end`}>
         <progress className={`self-center h-[1.3rem] transition-all duration-500 ease-in-out mobile:w-[6rem] [&::-moz-progress-bar]:bg-white [&::-webkit-progress-bar]:rounded-full [&::-webkit-progress-bar]:bg-white [&::-webkit-progress-bar]:p-[2px_6px] mobile:[&::-webkit-progress-bar]:p-[2px_3px] [&::-webkit-progress-value]:rounded-full [&::-webkit-progress-value]:bg-[#261676]`}></progress>
-        <img className='max-w-[100%] self-center' src={heartImg} alt="health img" />
+        <img className='self-center' src={heartImg} alt="health img" />
       </div>
-    </div>
+    </section>
   )
 }
 
