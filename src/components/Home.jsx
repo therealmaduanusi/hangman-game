4d358ecfb0ff1e16de7eccbdf4ab633bc03452f3
Victor Maduanusi <maduanusivictor2020@gmail.com>
2025-05-10 10:55:20 +0100

    Home page completed✅, both styling components on all screen sizes as challenged and rendering the game categories ✅.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,16 +4,16 @@ import playImg from '../assets/icon-play.svg'
 import { Link } from 'react-router-dom'
 function Home({onHandleCategory}) {
   return (
-    <section className={`flex min-h-[776px] items-center max-w-[100%]`}>
-        <div className={`max-w-[592px] flex-1 mx-[auto] pt-[10rem] pb-[2rem] bg-gradient-to-b from-[#344ABA] to-[#001479da] rounded-[2rem] border-t border-t-[0.5rem] border-r border-r-[0.3rem] border-l border-l-[0.3rem] border-[#2463ff] border-b border-b-[0.5rem] border-b-[#261676]`}>
-            <div className={`relative max-w-[400px] mx-auto`}>
-                <img className={`absolute top-[-250px] `} src={homeImg} alt="Hangman Image Home section" />
+    <section className={`flex min-h-[100dvh] items-center max-w-[100%] max-sm:mx-[1rem]`}>
+        <div className={`relative max-w-[592px] h-[500px] flex-1 mx-[auto] mt-[3rem] bg-gradient-to-b from-[#344ABA] to-[#001479da] rounded-[72px] border-t border-t-[0.5rem] border-r border-r-[0.3rem] border-l border-l-[0.3rem] border-[#2463ff] border-b border-b-[0.5rem] border-b-[#261676]`}>
+            <div className={`absolute w-[100%] xs:top-[-100px] max-sm:top-[-65px] max-sm:px-[1.5rem]`}>
+                <img className={`mx-auto`} src={homeImg} alt="Hangman Image Home section" />
             </div>
-            <div onClick={onHandleCategory} className={`bg-gradient-to-b from-[#FE71FE] to-[#7199FF] shadow-pink-sh w-[10rem] h-[10rem] mx-[auto] rounded-[50%] flex justify-center cursor-pointer`}>
+            <div onClick={onHandleCategory} className={`playBtn bg-gradient-to-b from-[#FE71FE] to-[#7199FF] shadow-pink-sh w-[200px] h-[200px] mx-[auto] mt-[100px] rounded-[50%] flex justify-center cursor-pointer`}>
                 <img className='self-center' src={playImg} alt="Play image to start game" />
             </div>
             <Link className='text-center block mt-[3.5rem]' to='/rules'>
-                <button className={`bg-[#2463ff] text-[#fff] w-[15rem] mx-[auto] rounded-[2rem] border-t border-t-[0.3rem] border-r border-r-[0.3rem] border-l border-l-[0.3rem] border-[#4277fd] text-[2rem] hover:bg-[#4277fd] hover:border-[#2463ff]`}>How to Play</button>
+                <button className={`bg-[#2463ff] text-[#fff] w-[260px] h-[62px] mx-[auto] rounded-[2rem] border-t border-t-[0.3rem] border-r border-r-[0.3rem] border-l border-l-[0.3rem] border-[#4277fd] text-[2rem] hover:bg-[#4277fd] hover:border-[#2463ff]`}>How to Play</button>
             </Link>
         </div>
     </section>
