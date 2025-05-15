8d5fd605064ab93c1bba3e29865575c6cd49678e
Victor Maduanusi <maduanusivictor2020@gmail.com>
2025-05-15 21:33:41 +0100

    add disabled button based on a if the letter clicked exist in the guessed array

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 
-function Keyboard({onGuess, guessedLetters, letters}) {
-  let lettersArr = letters.split('');
-  
+function Keyboard({ onGuess, guessedLetters, letters }) {
+  let lettersArr = letters.split("");
+
   // console.log(letters);
-  
+
   return (
-    <div className={`letterContainer grid grid-cols-9 max gap-[0.5rem] md:gap-[1rem]`}>
-      {lettersArr.map(letter => 
-        <button onClick={() => onGuess(letter)} className={` hover:bg-[#2463ff] py-[0.5rem] w-[95%] justify-self-center text-[#261676] uppercase text-[2rem] rounded-[20px] ${guessedLetters.includes(letter) ? 'bg-[#ffffff35] cursor-not-allowed hover:bg-[#ffffff35] hover:text-[#261676]' : 'bg-[#fff] hover:text-[#fff]'}`} key={letter}>
+    <div
+      className={`letterContainer grid grid-cols-9 max gap-[0.5rem] md:gap-[1rem]`}
+    >
+      {lettersArr.map((letter) => (
+        <button
+          onClick={() => onGuess(letter)}
+          className={` hover:bg-[#2463ff] py-[0.5rem] w-[95%] justify-self-center text-[#261676] uppercase text-[2rem] rounded-[20px] ${
+            guessedLetters.includes(letter)
+              ? "bg-[#ffffff35] cursor-not-allowed hover:bg-[#ffffff35] hover:text-[#261676]"
+              : "bg-[#fff] hover:text-[#fff]"
+          }`}
+          key={letter}
+          disabled={guessedLetters.includes(letter) ? true : false}
+        >
           {letter}
         </button>
-      )}
+      ))}
     </div>
   );
 }
